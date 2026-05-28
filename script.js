64e9c03e9a6207d745e97ff90a7e56b600d20991
linr5662 <linr5662@gmail.com>
2026-05-27 22:40:48 -0400

    week4 response

---
+++ script.js
@@ -0,0 +1,19 @@
+let myVariable = 6
+let myOtheVariable = 4 
+console.log(myVariable)
+
+let array =['apples','oranges',4,5,myVariable]
+
+console.log(array)
+
+console.log(array[0],array[1])
+console.log(array[1])
+
+array.push (myVariable)
+
+console.log(array)
+
+for(i= 0; i < 6; i++){
+    // console.log(i)
+    console.log(array[i])
+}   
